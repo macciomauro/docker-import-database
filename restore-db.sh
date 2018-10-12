f0a2e6863a13007786bb9164e7e84fd44622af41
Mauro Macciò <maccio.mauro@gmail.com>
2018-10-12 18:18:04 +0200

    Fix graphic stuff

--- restore-db.sh
+++ restore-db.sh
@@ -3,9 +3,9 @@
 cd ./
 
 if [ ! -f ".env" ]; then
-    echo "------------------------------------------------";
+    echo "-----------------------------------------------------";
     echo "[ERROR] .env file not found";
-    echo "------------------------------------------------";
+    echo "-----------------------------------------------------";
     exit 1;
 fi
 
@@ -13,15 +13,15 @@ export $(egrep -v '^#' .env | xargs)
 
 if [ -z "${DUMP_DIR}" ] || [ -z "${MYSQL_USER}" ] || [ -z "${MYSQL_PASSWORD}" ]
 then
-    echo "------------------------------------------------";
+    echo "-----------------------------------------------------";
     echo "[ERROR] Some .env values is missing or wrong";
-    echo "------------------------------------------------";
+    echo "-----------------------------------------------------";
     exit 1;
 fi
 
-echo "------------------------------------------------";
+echo "-----------------------------------------------------";
 echo "--- Welcome to the Docker Database Restore System ---";
-echo "------------------------------------------------";
+echo "-----------------------------------------------------";
 
 echo "Tell me the Docker container name and press [ENTER]:";
 read DATABASE_CONTAINER_NAME
@@ -39,41 +39,41 @@ then
     exit 1;
 fi
 
-echo "------------------------------------------------";
+echo "-----------------------------------------------------";
 echo "Choose a gzipped file (*.sql.gz) to dump";
 echo "ATTENTION! All previous data will be lost!";
-echo "------------------------------------------------";
+echo "-----------------------------------------------------";
 select FILENAME in "${DUMP_DIR}"/*.sql.gz;
 do
     if [[ $FILENAME = "" ]]; then
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "[ERROR] Empty Choise";
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         break;
         exit -1;
     else
         CHOSEN_DUMP=$(basename "$FILENAME");
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "You picked ${CHOSEN_DUMP}";
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "Start importing...";
         docker cp ${DUMP_DIR}/${CHOSEN_DUMP} ${DATABASE_CONTAINER_NAME}:/db_dump.sql.gz
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "Import completed";
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "Start dumping to ${MYSQL_DATABASE_NAME}...";
         docker exec ${DATABASE_CONTAINER_NAME} bash -c "mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} --default-character-set=utf8 -e 'DROP DATABASE IF EXISTS ${MYSQL_DATABASE_NAME}; CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'"
         docker exec ${DATABASE_CONTAINER_NAME} bash -c "zcat /db_dump.sql.gz | mysql -u ${MYSQL_USER} -p${MYSQL_PASSWORD} --default-character-set=utf8 ${MYSQL_DATABASE_NAME}"
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "Dump completed";
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "Removing import file...";
         docker exec ${DATABASE_CONTAINER_NAME} bash -c "rm /db_dump.sql.gz"
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "File removed";
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         echo "Bye!";
-        echo "------------------------------------------------";
+        echo "-----------------------------------------------------";
         break;
     fi
 done
